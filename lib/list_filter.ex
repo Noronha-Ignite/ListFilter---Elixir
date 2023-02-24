fe269ee260359e868467b01b6dca7850e422ec8e
Noronha1612 <inc.691@gmail.com>
2023-02-24 13:20:16 -0300

    Refactor main code structure

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -2,14 +2,19 @@ defmodule ListFilter do
   require Integer
 
   def call(list) do
-    listWithOnlyNumberStrs = Enum.filter(list, fn item -> String.match?(item, ~r/^\d+$/) end)
-
-    listWithParsedNumbers =
-      Enum.map(listWithOnlyNumberStrs, fn numberStr -> String.to_integer(numberStr) end)
-
-    listWithOnlyOddNumbers =
-      Enum.filter(listWithParsedNumbers, fn item -> Integer.is_odd(item) end)
+    listWithOnlyNumberStrs = filterOnlyNumStrings(list)
+    listWithParsedNumbers = parseNumStringsToIntegers(listWithOnlyNumberStrs)
+    listWithOnlyOddNumbers = filterOddNumbers(listWithParsedNumbers)
 
     Enum.count(listWithOnlyOddNumbers)
   end
+
+  defp filterOnlyNumStrings(list),
+    do: Enum.filter(list, fn item -> String.match?(item, ~r/^\d+$/) end)
+
+  defp parseNumStringsToIntegers(list),
+    do: Enum.map(list, fn numberStr -> String.to_integer(numberStr) end)
+
+  defp filterOddNumbers(list),
+    do: Enum.filter(list, fn item -> Integer.is_odd(item) end)
 end
